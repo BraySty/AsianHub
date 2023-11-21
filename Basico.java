63de4e9d03f5d72338d02639c7b3f8288a6bf352
alehqnw <ale120970@gmail.com>
2023-11-21 22:30:54 +0000

    Ventanas Update

--- Basico.java
+++
@@ -1,9 +0,0 @@
-public class Basico {
-    public static void main(String[] args) {
-        String nombre;
-        nombre = "JJOO";
-        for(int i = 0;i<5;i++){
-            System.out.println("HOLA "+nombre);
-        }
-    }
-}
